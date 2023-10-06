1176b8f07b5a43903a62c787378a88bc9eeaecea
unknown <omarmenjivar083@gmail.com>
2023-10-05 23:00:12 -0600

    Mi primer Comit

---
+++ src/main/java/com/examenes/sistemaapi/SistemaApiApplication.java
@@ -0,0 +1,13 @@
+package com.examenes.sistemaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaApiApplication.class, args);
+    }
+
+}
